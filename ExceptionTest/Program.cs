67a8bf69a7db229cfd66483eaa290a429ed47bf7
AnSantos99 <an.santos95@hotmail.com>
2021-10-26 10:00:01 +0100

    ExceptionTest Exercise

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insere um numero inteiro:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Numero inserido: {i}");
+        }
+    }
+}
