858f54c5e05a1ef2a9405facf0c83e5c98033aa7
AnSantos99 <an.santos95@hotmail.com>
2021-10-26 10:28:45 +0100

    add proper exceptions

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -16,9 +16,13 @@ namespace ExceptionTest
                 
                 
             }
-            catch(Exception e)
+            catch(FormatException e)
             {
                 Console.WriteLine("Format is 'int': " + e.Message);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("Number is greater or less than max and min value: " + e.Message);
             }
 
             Console.WriteLine($"Numero inserido: {i}");
