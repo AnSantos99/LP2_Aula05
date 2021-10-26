497d0f6ac942685cdc3a2eb3df3d9ec868b2b353
AnSantos99 <an.santos95@hotmail.com>
2021-10-26 10:21:14 +0100

    Catching Exceptions

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,9 +6,25 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
+            
+            int i = 0;
+            
             Console.WriteLine("Insere um numero inteiro:");
-            int i = int.Parse(Console.ReadLine());
+            try
+            {
+                i = int.Parse(Console.ReadLine());
+                
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Format is 'int': " + e.Message);
+            }
+
             Console.WriteLine($"Numero inserido: {i}");
+            
+            
+            
         }
     }
 }
